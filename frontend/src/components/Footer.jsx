e83592743c3fe7de19f9ef0ee2076c9ef5910cec
Tumaini Muriithi <wendomuriithi@gmail.com>
2026-07-30 21:31:07 +0300

    Created Footer css styling

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../css/Footer.css';
 
 function Footer() {
   const initiateChat = () => {
